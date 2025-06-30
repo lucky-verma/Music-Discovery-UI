fda45d531b351f5fac28bdfa0bd1efbf2ecc691a
Lucky Verma <thinkersloop@gmail.com>
2025-06-30 16:37:29 -0500

    fixed `__init__.py` for all modules

--- pages/__init__.py
+++ pages/__init__.py
@@ -10,4 +10,4 @@ This package contains individual page modules:
 from .discovery import render_discovery_page
 from .import_page import render_import_page
 
-__all__ = ["render_discovery_page", "render_import_page"]
+__all__ = ['render_discovery_page', 'render_import_page']
--- services/__init__.py
+++ services/__init__.py
@@ -3,7 +3,7 @@ Music Discovery Services
 
 This package contains all external service integrations:
 - Spotify API service
-- YouTube/yt-dlp service
+- YouTube/yt-dlp service  
 - Background job management
 """
 
@@ -11,4 +11,4 @@ from .spotify_service import SpotifyService
 from .youtube_service import YouTubeService
 from .job_service import JobManager
 
-__all__ = ["SpotifyService", "YouTubeService", "JobManager"]
+__all__ = ['SpotifyService', 'YouTubeService', 'JobManager']
